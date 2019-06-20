921bb41748cad3ca08293dc3ca6f6c031d36bc04
John Surina <gunneysurina@hotmail.com>
2019-06-20 17:01:12 -0500

    continue working on router portion of pracice application

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,16 @@
 import { NgModule } from '@angular/core';
 import {RouterModule, Routes} from '@angular/router';
+import { HeroesComponent } from './heroes/heroes.component';
+import {DashboardComponent} from './dashboard/dashboard.component';
 
 @NgModule({
-  imports: [],
+  imports: [
+    RouterModule.forRoot([
+      {path: 'heroes', component: HeroesComponent},
+      {path: 'dashboard', component: DashboardComponent},
+      {path: '', redirectTo:'dashboard', pathMatch:'full'}
+    ]),
+  ],
   exports: [
     RouterModule
   ]
