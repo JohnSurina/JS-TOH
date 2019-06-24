65f5d485921f04b23d10e7b9e51ddd22ec9d0eaf
JohnSurina <gunneysurina@hotmail.com>
2019-06-23 21:36:52 -0400

    finish router tasks per Angular.io

--- src/app/hero.service.ts
+++ src/app/hero.service.ts
@@ -1,7 +1,7 @@
-import { Injectable } from '@angular/core';
+import { Injectable, SystemJsNgModuleLoader } from '@angular/core';
 import { Hero } from './hero';
 import { myMockHeroes } from './mock-heroes';
-import {Observable, of} from 'rxjs';
+import { Observable, of } from 'rxjs';
 import { MessageService } from './message.service';
 
 @Injectable({
@@ -16,6 +16,21 @@ export class HeroService {
 getHeroes(): Observable<Hero[]> {
   this.myMessageService.add('HeroService: fetched heroes');
   return of(myMockHeroes);
+}
+
+getHero(searchID:number) : Observable<Hero> {
+  var returnHero:Hero = null;
+  myMockHeroes.forEach(element => {
+    if(element.id==searchID){
+      returnHero=element;
+    }
+  });
+  if(returnHero != null){
+    this.myMessageService.add(`HeroService: fetched hero @ searchID=${searchID}`);
+    return of(returnHero);
+  } else {
+    console.log('error: no matching hero ID');
+  }
 }
 
 }
